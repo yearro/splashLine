8d38737dea37ea5562e68cb3bff8502bb077c575
Yeri Armenta <yeri.armenta@gmail.com>
2026-06-19 15:18:35 -0600

    feat: implement custom color palette in tailwind configuration and update styles in index screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,11 +4,12 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 const index = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text className='text-3xl font-bold text-blue-500'>Hola Mundo</Text>
-        <Text className='text-2xl' style={{ fontFamily: 'inter-black' }}>Hola Mundo</Text>
-        <Text className='text-2xl font-inter-light'>Hola Mundo</Text>
-        <Text className='text-2xl' style={{ fontFamily: 'inter-medium' }}>Hola Mundo</Text>
+      <View className='bg-background'>
+        <Text className='text-3xl font-bold text-primary'>Hola Mundo</Text>
+        <Text className='text-2xl bg-secondary' style={{ fontFamily: 'inter-black' }}>Hola Mundo</Text>
+        <Text className='text-2xl font-inter-light text-slate-400'>Hola Mundo</Text>
+        <Text className='text-2xl bg-cyan-100/50' style={{ fontFamily: 'inter-medium' }}>Hola Mundo</Text>
+        <Text className='text-2xl bg-tertiary text-color_text' style={{ fontFamily: 'inter-medium' }}>Hola Mundo</Text>
       </View>
     </SafeAreaView>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        'primary': '#008190',
+        'secondary': '#9cf0ff',
+        'background': '#f8f9ff',
+        'tertiary': '#dbe9fe',
+        'color_text': '#0f1c2c',
+      },
       fontFamily: {
         'inter-black': ['Inter_18pt-Black', 'sans-serif'],
         'inter-light': ['Inter_18pt-Light', 'sans-serif'],
