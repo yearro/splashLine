eaf6acf78dcb47191184a1336d4311aa7706f92d
Yeri Armenta <yeri.armenta@gmail.com>
2026-07-08 14:41:41 -0600

    feat: add settings menu buttons and icon dependency for business and account configurations

--- src/app/(tabs)/settings/index.tsx
+++ src/app/(tabs)/settings/index.tsx
@@ -1,15 +1,27 @@
 import Footer from '@/components/Footer';
 import GeneralView from '@/components/GeneralView';
 import UserCard from '@/components/UserCard';
-import { StyleSheet, Text, View } from 'react-native';
+import { MaterialIcons } from '@react-native-vector-icons/material-icons/static';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 
 const SettingsScreen = () => {
   return (
     <GeneralView>
       <UserCard userName='Carlos Rodríguez' />
-      <View>
+      <View style={{ marginBottom: 20 }}>
         <Text style={styles.sectionTitle}>Settings</Text>
         <Text style={styles.sectionDescription}>Manage your account preferences and business configurations.</Text>
+      </View>
+      <Text style={styles.smText}>BUSINESS</Text>
+      <View style={styles.buttonsContainer}>
+        <Pressable style={styles.button}></Pressable>
+        <Pressable style={styles.button}></Pressable>
+      </View>
+      <Text style={styles.smText}>ACCOUNT</Text>
+      <View style={styles.buttonsContainer}>
+        <Pressable style={styles.button}>
+          <MaterialIcons name='local-car-wash' size={24} color='black' />
+        </Pressable>
       </View>
       <Footer />
     </GeneralView>
@@ -27,5 +39,27 @@ const styles = StyleSheet.create({
   sectionDescription: {
     fontFamily: 'Inter_18pt-Light',
     fontSize: 14,
+  },
+  smText: {
+    fontFamily: 'Inter_18pt-Light',
+    fontSize: 12,
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+    gap: 10,
+    justifyContent: 'space-between',
+  },
+  button: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 15,
+    paddingVertical: 20,
+    borderRadius: 15,
+    borderWidth: 1,
+    borderColor: 'rgba(0,0,0,0.1)',
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+    height: 150,
   }
 })
