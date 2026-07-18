07327978621738f172c331072299a165feda8655
Yeri Armenta <yeri.armenta@gmail.com>
2026-07-17 18:58:23 -0600

    feat: implement form handling with Formik and Yup validation for service creation and editing

--- src/app/(tabs)/settings/services/[id].tsx
+++ src/app/(tabs)/settings/services/[id].tsx
@@ -1,20 +1,14 @@
 import GeneralView from '@/components/GeneralView';
 import Colors from '@/constants/Colors';
+import { descriptionSchema, nameSchema, priceSchema } from '@/validations';
 import Ionicons from '@react-native-vector-icons/ionicons';
 import { useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
+import { Formik } from 'formik';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import * as yup from 'yup';
 
 const IdServiceScreen = () => {
   const { id } = useLocalSearchParams();
-  const [serviceName, setServiceName] = useState('');
-  const [servicePrice, setServicePrice] = useState('');
-  const [serviceDescription, setServiceDescription] = useState('');
-
-  const handleNumericChange = (text: string) => {
-    const numericValue = text.replace(/[^0-9]/g, '');
-    setServicePrice(numericValue);
-  };
 
   return (
     <GeneralView>
@@ -22,35 +16,64 @@ const IdServiceScreen = () => {
         <Text style={styles.title}>{id === '0' ? 'Add service' : 'Edit service'}</Text>
         <Text style={styles.sectionDescription}>Manage your account preferences and business configurations.</Text>
       </View>
-      <TextInput
-        placeholder="Service name"
-        style={styles.input}
-        value={serviceName}
-        onChangeText={setServiceName}
-      />
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Ionicons style={{ marginBottom: 20 }} name="cash-outline" size={24} color={Colors.primary} />
-        <View style={{ flex: 1, marginLeft: 10 }}>
-          <TextInput
-            placeholder="Price"
-            keyboardType="numeric"
-            style={styles.input}
-            value={servicePrice}
-            onChangeText={handleNumericChange}
-          />
-        </View>
-      </View>
-      <TextInput
-        multiline={true}
-        numberOfLines={4}
-        placeholder="Service description"
-        style={[styles.input, { textAlignVertical: 'top', height: 'auto', minHeight: 100 }]}
-        value={serviceDescription}
-        onChangeText={setServiceDescription}
-      />
-      <TouchableOpacity style={styles.button} onPress={() => { }}>
-        <Text style={styles.buttonText}>{id === '0' ? 'Add service' : 'Save service'}</Text>
-      </TouchableOpacity>
+      <Formik
+        initialValues={{
+          serviceName: '',
+          servicePrice: 0,
+          serviceDescription: '',
+        }}
+        validationSchema={yup.object({
+          serviceName: nameSchema,
+          servicePrice: priceSchema,
+          serviceDescription: descriptionSchema,
+        })}
+        onSubmit={(values) => {
+          console.log(values);
+        }}
+      >
+        {props => (
+          <View>
+            <TextInput
+              placeholder="Service name"
+              style={styles.input}
+              value={props.values.serviceName}
+              onChangeText={props.handleChange('serviceName')}
+            />
+            {props.touched.serviceName && props.errors.serviceName && (
+              <Text style={{ color: 'red' }}>{props.errors.serviceName}</Text>
+            )}
+            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Ionicons style={{ marginBottom: 20 }} name="cash-outline" size={24} color={Colors.primary} />
+              <View style={{ flex: 1, marginLeft: 10 }}>
+                <TextInput
+                  placeholder="Price"
+                  keyboardType="numeric"
+                  style={styles.input}
+                  value={props.values.servicePrice.toString()}
+                  onChangeText={props.handleChange('servicePrice')}
+                />
+              </View>
+            </View>
+            {props.touched.servicePrice && props.errors.servicePrice && (
+              <Text style={{ color: 'red' }}>{props.errors.servicePrice}</Text>
+            )}
+            <TextInput
+              multiline={true}
+              numberOfLines={4}
+              placeholder="Service description"
+              style={[styles.input, { textAlignVertical: 'top', height: 'auto', minHeight: 100 }]}
+              value={props.values.serviceDescription}
+              onChangeText={props.handleChange('serviceDescription')}
+            />
+            {props.touched.serviceDescription && props.errors.serviceDescription && (
+              <Text style={{ color: 'red' }}>{props.errors.serviceDescription}</Text>
+            )}
+            <TouchableOpacity style={styles.button} onPress={() => props.handleSubmit()}>
+              <Text style={styles.buttonText}>{id === '0' ? 'Add service' : 'Save service'}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      </Formik>
     </GeneralView>
   );
 }
---
+++ src/validations/index.ts
@@ -0,0 +1,17 @@
+import * as yup from 'yup';
+
+export const priceSchema = yup.number()
+  .required('Price is required')
+  .positive('Must be positive')
+  .min(1, 'Minimum value is 1')
+  .typeError('You must enter a valid number');
+
+export const nameSchema = yup.string()
+  .required('Name is required')
+  .min(3, 'Name must be at least 3 characters')
+  .max(20, 'Name must be at most 20 characters');
+
+export const descriptionSchema = yup.string()
+  .required('Description is required')
+  .min(10, 'Description must be at least 10 characters')
+  .max(1000, 'Description must be at most 1000 characters');
